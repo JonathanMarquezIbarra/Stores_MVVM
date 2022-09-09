00520964135b06b7b5f5f1a2cd8a30a52147f051
JonathanMarquezIbarra <jonathan.marquib@gmail.com>
2022-09-08 22:31:11 -0500

    Initial commit

---
+++ app/src/main/java/com/cursosant/android/stores/mainModule/model/MainInteractor.kt
@@ -0,0 +1,36 @@
+package com.cursosant.android.stores.mainModule.model
+
+import com.cursosant.android.stores.StoreApplication
+import com.cursosant.android.stores.common.entities.StoreEntity
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+
+class MainInteractor {
+
+    fun getStores(callback: (MutableList<StoreEntity>) -> Unit){
+        doAsync {
+            val storeList = StoreApplication.database.storeDao().getAllStores()
+            uiThread {
+                callback(storeList)
+            }
+        }
+    }
+
+    fun deleteStore(storeEntity: StoreEntity, callback: (StoreEntity) -> Unit){
+        doAsync {
+            StoreApplication.database.storeDao().deleteStore(storeEntity)
+            uiThread {
+                callback(storeEntity)
+            }
+        }
+    }
+
+    fun updateStore(storeEntity: StoreEntity, callback: (StoreEntity) -> Unit){
+        doAsync {
+            StoreApplication.database.storeDao().updateStore(storeEntity)
+            uiThread {
+                callback(storeEntity)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/cursosant/android/stores/mainModule/viewModel/MainViewModel.kt
@@ -0,0 +1,56 @@
+package com.cursosant.android.stores.mainModule.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.cursosant.android.stores.common.entities.StoreEntity
+import com.cursosant.android.stores.mainModule.model.MainInteractor
+
+class MainViewModel:ViewModel() {
+    private var storeList: MutableList<StoreEntity>
+    private var interactor: MainInteractor
+
+    init {
+        storeList = mutableListOf()
+        interactor = MainInteractor()
+        //stores = MutableLiveData()
+    }
+
+    private val stores: MutableLiveData<List<StoreEntity>> by lazy {
+        MutableLiveData<List<StoreEntity>>().also {
+            loadStores()
+        }
+    }
+
+    fun getStores(): LiveData<List<StoreEntity>>{
+        return stores
+    }
+
+    private fun loadStores(){
+        interactor.getStores {
+            stores.value = it
+            storeList = it
+        }
+    }
+
+    fun deleteStore(storeEntity: StoreEntity){
+        interactor.deleteStore(storeEntity,{
+            val index = storeList.indexOf(storeEntity)
+            if (index != -1){
+                storeList.removeAt(index)
+                stores.value = storeList
+            }
+        })
+    }
+
+    fun updateStore(storeEntity: StoreEntity){
+        storeEntity.isFavorite = !storeEntity.isFavorite
+        interactor.updateStore(storeEntity,{
+            val index = storeList.indexOf(storeEntity)
+            if (index != -1){
+                storeList.set(index, storeEntity)
+                stores.value = storeList
+            }
+        })
+    }
+}
